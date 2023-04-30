5177c2c67986f692970c1efa385e001c55722329
Martinz64 <24656265+Martinz64@users.noreply.github.com>
2023-04-30 09:37:29 +0200

    [NoCompression]Might work on iOS, fixed the pasting screenshot from keyboard bug(i think)

--- plugins/NoCompression/src/index.ts
+++ plugins/NoCompression/src/index.ts
@@ -6,18 +6,28 @@ let unpatchCompressor;
 
 export default {
     onLoad: () => {
-        const CloudUploaderBase = findByProps("stageAttachmentFiles");
+
+        //do i actually need this?
+        /*const CloudUploaderBase = findByProps("stageAttachmentFiles");
         unpatchUploader = before("stageAttachmentFiles", CloudUploaderBase, (args) => {
             let files = args[0];
             for (let i = 0; i < files.length; i++) {
                 files[i].item.uri = files[i].item.originalUri;
             }
-        });
+        });*/
 
         const CloudUpload = findByProps('CloudUpload').CloudUpload;
         unpatchCompressor = before('reactNativeCompressAndExtractData', CloudUpload.prototype, function() {
             this.reactNativeFilePrepped = true;
             this.currentSize = this.preCompressionSize;
+
+            //add extension to files that don't have one
+            if(this.mimeType == 'image/png' && !this.filename.endsWith(".png")){
+                this.filename = this.filename + '.png'
+            }
+            if(this.mimeType == 'image/jpg' && (!this.filename.endsWith(".jpg") || !this.filename.endsWith(".jpeg"))){
+                this.filename = this.filename + '.jpg'
+            }
         });
     },
     onUnload: () => {
