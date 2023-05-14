43025308387db21cb46298ab41b3770d733b2e5d
Martinz64 <24656265+Martinz64@users.noreply.github.com>
2023-05-14 21:43:24 +0200

    [PlatformIndicators]fix settings thing

--- plugins/PlatformIndicators/src/settings.tsx
+++ plugins/PlatformIndicators/src/settings.tsx
@@ -3,42 +3,39 @@ import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
 import { Forms } from "@vendetta/ui/components";
 
-const {View} = ReactNative;
+const {View, ScrollView} = ReactNative;
 
 export default function Settings() {
     useProxy(storage);
 
     return (
-        <View>
-            <Forms.FormSwitchRow
-                label="DM Top Bar"
-                subLabel="Show icons on the dm top bar"
-                value={storage.dmTopBar ?? true}
-                onValueChange={v => storage.dmTopBar = v}
-                note=""
-            />
-            <Forms.FormSwitchRow
-                label="Users/DMs list"
-                subLabel="Show icons on the users and DMs list"
-                value={storage.userList ?? true}
-                onValueChange={v => storage.userList = v}
-                note=""
-            />
-            <Forms.FormSwitchRow
-                label="Profile name"
-                subLabel="Show icons on user profiles"
-                value={storage.profileUsername ?? true}
-                onValueChange={v => storage.profileUsername = v}
-                note=""
-            />
-            <Forms.FormSwitchRow
-                label="Disable mobile icon"
-                subLabel="Hide mobile status from the normal indicator"
-                value={storage.removeDefaultMobile ?? true}
-                onValueChange={v => storage.removeDefaultMobile = v}
-                note=""
-            />
-        </View>
-        
+        <ScrollView>
+            <View>
+                <Forms.FormSwitchRow
+                    label="Show icons on the dm top bar"
+                    value={storage.dmTopBar ?? true}
+                    onValueChange={v => storage.dmTopBar = v}
+                    note=""
+                />
+                <Forms.FormSwitchRow
+                    label="Show icons on the users and DMs list"
+                    value={storage.userList ?? true}
+                    onValueChange={v => storage.userList = v}
+                    note=""
+                />
+                <Forms.FormSwitchRow
+                    label="Show icons on user profiles"
+                    value={storage.profileUsername ?? true}
+                    onValueChange={v => storage.profileUsername = v}
+                    note=""
+                />
+                <Forms.FormSwitchRow
+                    label="Hide mobile status from the normal indicator"
+                    value={storage.removeDefaultMobile ?? true}
+                    onValueChange={v => storage.removeDefaultMobile = v}
+                    note=""
+                />
+            </View>
+        </ScrollView>
     );
 }
